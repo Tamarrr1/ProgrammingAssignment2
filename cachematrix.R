38afa3e634ca00131bbdb1217647884a26d4b024
Tamarrr1 <tamar.kastelein@macaw.nl>
2015-06-21 12:16:18 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This the code for the assignment
 
-## Write a short comment describing this function
-
+## This function adds the data into cache memory
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-#dit is het werk van Tamar
-
-## Write a short comment describing this function
-
+## This is a funtion that returns the inverse matrix of 'x'
+## If the matrix had been inverted already, the funtion uses cached data
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+# This creates a matrix
+cells <- c(1,26,24,68)
+rnames <- c("R1", "R2")
+cnames <- c("C1", "C2") 
+mymatrix <- matrix(cells, nrow=2, ncol=2, byrow=TRUE,
+                   dimnames=list(rnames, cnames))
+
+# Here can you test the functions
+test <- makeCacheMatrix(mymatrix)
+cacheSolve(test)
+cacheSolve(test)
